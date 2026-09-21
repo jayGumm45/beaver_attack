2f55c1a58c15b7c039ffde2faee2cc0a8528150c
LTolbert <tolbertlw@gmail.com>
2026-09-20 21:35:54 -0500

    Added collision and movement, adjusted player speed, added a main node, and a few more housekeeping things

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 120
+@export var speed = 200
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
@@ -10,4 +10,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position+=velocity*delta
+	move_and_slide()
