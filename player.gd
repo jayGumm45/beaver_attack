bff5e7cdbadba1123ee1e289ed39257971fc5113
LTolbert <tolbertlw@gmail.com>
2026-09-22 14:57:46 -0500

    Changed player size

--- player.gd
+++ player.gd
@@ -34,8 +34,7 @@ func _process(delta):
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "Walk"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedSprite2D.flip_h = velocity.x > 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "Up"
 
