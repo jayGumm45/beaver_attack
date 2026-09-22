440fd4b0b895237be0727808f97c183ead7c3c9d
Caleb <calab.a.andrews123@gmail.com>
2026-09-22 11:25:22 -0500

    Movement needs to be fixed. Fix character shaking.

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 400
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
@@ -13,12 +13,24 @@ func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -27,3 +39,5 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.stop()
 	position+=velocity*delta
 	move_and_slide()
+	
+	
