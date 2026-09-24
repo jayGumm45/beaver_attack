c3f77817ce63e72ca40bdc70f3a0fa0e165d8890
Caleb <calab.a.andrews123@gmail.com>
2026-09-24 14:27:32 -0500

    Added Script to main & Created timer child nods

---
+++ main(tilemap).gd
@@ -0,0 +1,22 @@
+extends Node
+
+var timer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func game_over():
+	$CountdownTimer.stop()
+
+func new_game():
+	timer = 10
+	$Player.start($StartPosition.position)
+	$CountdownTimer.start()
+	
+func _on_count_down_timeout():
+	timer -= 1
